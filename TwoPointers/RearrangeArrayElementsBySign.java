aea1a836203ce7ed1999555ccee29374cbb56f29
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-14 23:00:11 +0700

    2149. Rearrange Array Elements by Sign

---
+++ TwoPointers/RearrangeArrayElementsBySign.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        // Positive index
+        int i = 0;
+        // Negative index
+        int j = 1;
+
+        for (int num : nums) {
+            if (num > 0) {
+                result[i] = num;
+                i += 2;
+            } else {
+                result[j] = num;
+                j += 2;
+            }
+        }
+        return result;
+    }
+}
