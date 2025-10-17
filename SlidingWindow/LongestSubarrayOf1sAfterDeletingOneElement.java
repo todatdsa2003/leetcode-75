c0457a584411e0cd42578387b9c77490beb6ffa8
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-18 05:29:34 +0700

    Update Format 1493. Longest Subarray of 1's After Deleting One Element

--- SlidingWindow/LongestSubarrayOf1sAfterDeletingOneElement.java
+++ SlidingWindow/LongestSubarrayOf1sAfterDeletingOneElement.java
@@ -1,33 +1,24 @@
-public class LongestSubarrayOf1sAfterDeletingOneElement {
-    class Solution {
-        public int longestSubarray(int[] nums) {
-            int windowStart = 0;
-            int maxLength = 0;
-            int count = 0;
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int windowStart = 0;
+        int maxLength = 0;
+        int count = 0;
 
-            for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
-                if (nums[windowEnd] == 0) {
-                    count++;
-                }
-
-                while (count > 1) {
-                    if (nums[windowStart] == 0) {
-                        count--;
-                    }
-                    windowStart++;
-                }
-
-                maxLength = Math.max(maxLength, windowEnd - windowStart);
+        for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+            if (nums[windowEnd] == 0) {
+                count++;
             }
 
-            return maxLength;
-        }
-    }
+            while (count > 1) {
+                if (nums[windowStart] == 0) {
+                    count--;
+                }
+                windowStart++;
+            }
 
-    public static void main(String[] args) {
-        Solution solution = new LongestSubarrayOf1sAfterDeletingOneElement().new Solution();
-        int[] nums = {1, 1, 0, 1, 1, 1, 1, 1, 1};
-        int result = solution.longestSubarray(nums);
-        System.out.println("Longest subarray of 1s after deleting one element: " + result);
+            maxLength = Math.max(maxLength, windowEnd - windowStart);
+        }
+
+        return maxLength;
     }
 }
