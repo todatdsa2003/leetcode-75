93b201c9a8674c9a754f9ed9bacadcd59de2f2ef
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-19 22:51:24 +0700

    Update format code 2269. Find the K-Beauty of a Number

--- PracticeSlidingWindow/FindTheKBeautyOfANumber.java
+++ PracticeSlidingWindow/FindTheKBeautyOfANumber.java
@@ -1,28 +1,18 @@
-public class FindTheKBeautyOfANumber {
-    class Solution {
-        public int divisorSubstrings(int num, int k) {
-            String s = Integer.toString(num);
-            int n = s.length();
-            int count = 0; 
+class Solution {
+    public int divisorSubstrings(int num, int k) {
+        String s = Integer.toString(num);
+        int n = s.length();
+        int count = 0;
 
-            for (int i = 0; i <= n - k; i++) {
-                String sub = s.substring(i, i + k);
-                int divisor = Integer.parseInt(sub);
+        for (int i = 0; i <= n - k; i++) {
+            String sub = s.substring(i, i + k);
+            int divisor = Integer.parseInt(sub);
 
-                if (divisor != 0 && num % divisor == 0) {
-                    count++;
-                }
+            if (divisor != 0 && num % divisor == 0) {
+                count++;
             }
-
-            return count;
         }
-    }
 
-    public static void main(String[] args) {
-        Solution solution = new FindTheKBeautyOfANumber().new Solution();
-        int num = 240;
-        int k = 2;
-        int result = solution.divisorSubstrings(num, k);
-        System.out.println("The K-Beauty of the number is: " + result);
+        return count;
     }
 }
