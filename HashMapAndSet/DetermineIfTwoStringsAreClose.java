9a1a00561b8fc99f91df1b3c1aa7d3d16849b5a4
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-23 14:01:18 +0700

    1657. Determine if Two Strings Are Close

---
+++ HashMapAndSet/DetermineIfTwoStringsAreClose.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class DetermineIfTwoStringsAreClose {
+    class Solution {
+        public boolean closeStrings(String word1, String word2) {
+            // Neu do dai khac nhau thi khong the gan nhau
+            if (word1.length() != word2.length()) {
+                return false;
+            }
+
+            // Dem tan so xuat hien cua tung ky tu trong hai chuoi
+            int[] count1 = new int[26];
+            int[] count2 = new int[26];
+
+            for (char c : word1.toCharArray()) {
+                count1[c - 'a']++;
+            }
+
+            for (char c : word2.toCharArray()) {
+                count2[c - 'a']++;
+            }
+
+            // Kiem tra co cung ky tu hay khong
+            for (int i = 0; i < 26; i++) {
+                if ((count1[i] == 0 && count2[i] != 0) || (count1[i] != 0 && count2[i] == 0)) {
+                    return false;
+                }
+            }
+
+            // Sap xep tan so xuat hien va so sanh
+            Arrays.sort(count1);
+            Arrays.sort(count2);
+
+            return Arrays.equals(count1, count2);
+        }
+    }
+    
+}
