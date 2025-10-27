e7ea70334475a122d3235c1ffbcbe6e6b3a2a9e2
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-27 19:08:36 +0700

    739. Daily Temperatures

---
+++ Stack/DailyTemperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        // Stack<Integer> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
