5f6ff8839e1bd4e69541f6b9d4805d75a80f79ee
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-28 18:50:04 +0700

    1441. Build an Array With Stack Operations

--- Stack/BuildAnArrayWithStackOperations.java
+++ Stack/BuildAnArrayWithStackOperations.java
@@ -1,22 +1,16 @@
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class BuildAnArrayWithStackOperations {
-    class Solution {
-        public List<String> buildArray(int[] target, int n) {
-            List<String> result = new ArrayList<>();
-            int currentNumber = 1;
-            for (int number : target) {
-                while (currentNumber < number) {
-                    result.add("Push");
-                    result.add("Pop");
-                    currentNumber++;
-                }
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> result = new ArrayList<>();
+        int currentNumber = 1;
+        for (int number : target) {
+            while (currentNumber < number) {
                 result.add("Push");
+                result.add("Pop");
                 currentNumber++;
             }
-            return result;
+            result.add("Push");
+            currentNumber++;
         }
+        return result;
     }
 }
