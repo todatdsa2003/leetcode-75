cbf3bdb3a88129eaa0bab49e08f7d8e339955ac9
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-28 18:50:42 +0700

    901. Online Stock Span

--- Stack/OnlineStockSpan.java
+++ Stack/OnlineStockSpan.java
@@ -1,29 +1,24 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+class StockSpanner {
+    private Deque<int[]> stack;
 
-public class OnlineStockSpan {
-    class StockSpanner {
-        private Deque<int[]> stack;
-
-        public StockSpanner() {
-            stack = new ArrayDeque<>();
-        }
-
-        public int next(int price) {
-            int span = 1;
-            while (!stack.isEmpty() && price >= stack.peek()[0]) {
-                span += stack.pop()[1]; 
-            }
-
-            stack.push(new int[]{price, span});
-            return span;
-        }
-       
+    public StockSpanner() {
+        stack = new ArrayDeque<>();
     }
 
-    /**
-     * Your StockSpanner object will be instantiated and called as such:
-     * StockSpanner obj = new StockSpanner();
-     * int param_1 = obj.next(price);
-     */
+    public int next(int price) {
+        int span = 1;
+        while (!stack.isEmpty() && price >= stack.peek()[0]) {
+            span += stack.pop()[1];
+        }
+
+        stack.push(new int[] { price, span });
+        return span;
+    }
+
 }
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
