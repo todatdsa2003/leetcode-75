b7bfb7925625f5054342de44e28de438afed65a0
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-10-29 21:29:59 +0700

    1700. Number of Students Unable to Eat Lunch

--- Queue/NumberOfStudentsUnableToEatLunch.java
+++ Queue/NumberOfStudentsUnableToEatLunch.java
@@ -1,29 +1,25 @@
-import java.util.*;
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> studentQueue = new LinkedList<>();
 
-public class NumberOfStudentsUnableToEatLunch {
-    class Solution {
-        public int countStudents(int[] students, int[] sandwiches) {
-            Queue<Integer> studentQueue = new LinkedList<>();
-
-            for (int student : students) {
-                studentQueue.offer(student);
-            }
-
-            int i = 0; 
-            int count = 0;
-
-            while (!studentQueue.isEmpty() && count < studentQueue.size()) {
-                int currentStudent = studentQueue.poll();
-                if (currentStudent == sandwiches[i]) {
-                    i++; 
-                    count = 0; 
-                } else {
-                    studentQueue.offer(currentStudent);
-                    count++; 
-                }
-            }
-
-            return studentQueue.size();
+        for (int student : students) {
+            studentQueue.offer(student);
         }
+
+        int i = 0;
+        int count = 0;
+
+        while (!studentQueue.isEmpty() && count < studentQueue.size()) {
+            int currentStudent = studentQueue.poll();
+            if (currentStudent == sandwiches[i]) {
+                i++;
+                count = 0;
+            } else {
+                studentQueue.offer(currentStudent);
+                count++;
+            }
+        }
+
+        return studentQueue.size();
     }
 }
