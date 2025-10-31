6e5290ef278588b766f7c8bcd918bfc21b4024ab
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-01 03:39:56 +0700

    872. Leaf-Similar Trees

---
+++ BinaryTreeDFS/LeafSimilarTrees.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+
+        dfs(root1, l1);
+        dfs(root2, l2);
+
+        return l1.equals(l2);
+    }
+
+    private void dfs(TreeNode node, List<Integer> leaves) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            leaves.add(node.val);
+            return;
+        }
+
+        dfs(node.left, leaves);
+        dfs(node.right, leaves);
+    }
+}
