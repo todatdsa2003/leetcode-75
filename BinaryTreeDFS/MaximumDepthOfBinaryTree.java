132a24a4c24f337936d1ccac95df1ede7faeba1c
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-01 03:13:25 +0700

    104. Maximum Depth of Binary Tree

--- BinaryTreeDFS/MaximumDepthOfBinaryTree.java
+++ BinaryTreeDFS/MaximumDepthOfBinaryTree.java
@@ -1,16 +1,16 @@
 /**
- * Định nghĩa của một nút trong cây (LeetCode đã cung cấp sẵn):
+ * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 
