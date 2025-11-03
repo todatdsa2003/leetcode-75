3e31ff16ba377aa0e02deaf82148352d6bbc9a64
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-03 23:09:47 +0700

    113. Path Sum II

---
+++ BinaryTreeDFS/PathSumII.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return dfs(root.left, true) + dfs(root.right, false);
+    }
+
+    private int dfs(TreeNode node, boolean isLeft) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            if (isLeft) {
+                return node.val;
+            } else {
+                return 0;
+            }
+        }
+
+        int leftSum = dfs(node.left, true);
+        int rightSum = dfs(node.right, false);
+        return leftSum + rightSum;
+    }
+}
