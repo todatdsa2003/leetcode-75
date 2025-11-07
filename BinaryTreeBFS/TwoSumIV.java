897ef395cfe58664d6ac3b959d19c4a7b929985f
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-08 00:28:32 +0700

    653. Two Sum IV

--- BinaryTreeBFS/TwoSumIV.java
+++ BinaryTreeBFS/TwoSumIV.java
@@ -1,3 +1,41 @@
-public class TwoSumIV {
-    
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<>();
+        inorder(root, arr);
+        int l = 0, r = arr.size() - 1;
+
+        while (l < r) {
+            long sum = (long) arr.get(l) + arr.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
+        }
+        return false;
+    }
+
+    private void inorder(TreeNode node, List<Integer> arr) {
+        if (node == null)
+            return;
+        inorder(node.left, arr);
+        arr.add(node.val);
+        inorder(node.right, arr);
+    }
 }
