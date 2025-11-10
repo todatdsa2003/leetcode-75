adf39ca4aa39c641806968d6d33b8886a114bb77
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-10 23:46:48 +0700

    Update: Approach 2: Using Iterative (Call Stack)

--- BinaryTreeDFS/BinaryTreeInorderTraversal.java
+++ BinaryTreeDFS/BinaryTreeInorderTraversal.java
@@ -14,19 +14,40 @@
  * }
  */
 class Solution {
+    /*Approach 1: Using Recursive */
+    // public List<Integer> inorderTraversal(TreeNode root) {
+    //     List<Integer> result = new ArrayList<>();
+    //     dfs(root, result);
+    //     return result;
+    // }
+
+    // private void dfs(TreeNode node, List<Integer> result) {
+    //     if (node == null) {
+    //         return;
+    //     }
+    
+    //     dfs(node.left, result);
+    //     result.add(node.val);
+    //     dfs(node.right, result);
+    // }
+
+    /*Approach 2: Using Iterative (Call Stack) */
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        dfs(root, result);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
         return result;
     }
-
-    private void dfs(TreeNode node, List<Integer> result) {
-        if (node == null) {
-            return;
-        }
     
-        dfs(node.left, result);
-        result.add(node.val);
-        dfs(node.right, result);
-    }
 }
