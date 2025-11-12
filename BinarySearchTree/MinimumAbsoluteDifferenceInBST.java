5bfd4827ea3f70267192ec3d929974c92cb59af8
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-12 11:03:27 +0700

    530. Minimum Absolute Difference in BST

---
+++ BinarySearchTree/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Integer prev = null;
+    private int min = Integer.MAX_VALUE;
+
+    public int getMinimumDifference(TreeNode root) {
+        inorder(root);
+        return min;
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) return;
+
+        inorder(node.left);
+        if (prev != null) {
+            int diff = node.val - prev;
+            if (diff < min) {
+                min = diff;
+            }
+        }
+        prev = node.val;
+        inorder(node.right);
+    }
+}
+
+
