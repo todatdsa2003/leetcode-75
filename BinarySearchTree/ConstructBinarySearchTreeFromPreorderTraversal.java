82c209cbff766d322f1901e0761f56ae2855ff16
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-13 21:17:01 +0700

    1008. Construct Binary Search Tree from Preorder Traversal

---
+++ BinarySearchTree/ConstructBinarySearchTreeFromPreorderTraversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+class Solution {
+    private int index = 0;
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, Integer.MAX_VALUE);
+    }
+
+    private TreeNode build(int[] preorder, int bound) {
+        if (index == preorder.length || preorder[index] > bound) {
+            return null;
+        }
+        int val = preorder[index++];
+        TreeNode root = new TreeNode(val);
+        root.left = build(preorder, val);
+        root.right = build(preorder, bound);
+
+        return root;
+    }
+}
