7e9facccc98a91bcca589cc2379f648d2a9f2670
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-17 15:46:04 +0700

    841. Keys and Rooms

---
+++ GraphDFS/KeysAndRooms.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int n = rooms.size();
+        boolean[] visited = new boolean[n];
+        dfs(0, rooms, visited);
+        for (boolean v : visited) {
+            if (!v)
+                return false;
+        }
+        return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+        if (visited[room])
+            return;
+        visited[room] = true;
+        for (int key : rooms.get(room)) {
+            dfs(key, rooms, visited);
+        }
+    }
+
+}
