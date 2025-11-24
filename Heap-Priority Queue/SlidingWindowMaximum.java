7ef05f7d03b81c2bb2ac5399b68e6770d6bd95b3
todatdsa2003 <todat.dsa.2003@gmail.com>
2025-11-24 23:49:11 +0700

    239. Sliding Window Maximum - not complete

---
+++ Heap-Priority Queue/SlidingWindowMaximum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+
+        for (int i = 0; i < k; i++)
+            pq.offer(new int[] { nums[i], i });
+        result[0] = pq.peek()[0];
+
+        for (int i = k; i < n; i++) {
+            pq.offer(new int[] { nums[i], i });
+            while (pq.peek()[1] <= i - k)
+                pq.poll();
+            result[i - k + 1] = pq.peek()[0];
+        }
+
+        return result;
+    }
+}
